5246f1aa3848065f0209bcb82254db229d909053
unknown <mattb.1099@gmail.com>
2022-08-04 21:59:35 -0500

    Homework for assignment 6

--- api_keys.py
+++ api_keys.py
@@ -2,4 +2,6 @@
 weather_api_key = "b52008c9f6e370eb9a14d81bc33ddb5f"
 
 # Google API Key
-g_key = "AIzaSyAI3SUQAwK7btHgGLH-BypvYSWh-QaCUHg"
+g_key = "AIzaSyBfqEKHNVbllVnrTHKh91sQfHgWmZuIKHk"
+
+#loc_key = "AIzaSyBHfGVrow3Oy0eeM9slXgnQ5408gi70eZU"
